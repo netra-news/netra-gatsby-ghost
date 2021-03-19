029b64cef8066853c9d753c3b24bb5b76ced0aba
netra-news <80890104+netra-news@users.noreply.github.com>
2021-03-19 14:17:31 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://netra.news`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://storage.googleapis.com/netra.news`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 20, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
